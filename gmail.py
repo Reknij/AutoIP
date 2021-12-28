ed3d6b38d143e6241d81a9840ff7eac994b718f7
JinkerLeong <jinker2003@gmail.com>
2021-12-28 19:53:50 +0800

    optimize variable to config

--- gmail.py
+++ gmail.py
@@ -1,27 +1,21 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP
+import configManager
 
-sender_address = ""
-sender_pass = ""
-receiver_address = ""
-def SetEmail(sender, senderp,receiver):
-    global sender_address, sender_pass, receiver_address
-    sender_address = sender
-    sender_pass = senderp
-    receiver_address = receiver
+config = configManager.getConfig()
 
 def SendGmail(subject,content):
     message = MIMEMultipart()
-    message['From'] = sender_address
-    message['To'] = receiver_address
+    message['From'] = config.sender
+    message['To'] = config.receiver
     message['Subject'] = subject  #The subject line
     #The body and the attachments for the mail
     message.attach(MIMEText(content, 'plain'))
     #Create SMTP session for sending the mail
     session = SMTP('smtp.gmail.com', 587) #use gmail with port
     session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
+    session.login(config.sender, config.senderPass) #login with mail_id and password
     text = message.as_string()
-    session.sendmail(sender_address, receiver_address, text)
+    session.sendmail(config.sender, config.receiver, text)
     session.quit()
