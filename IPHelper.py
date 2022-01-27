8d455fc75d8094eb24b86dd5951e2e6ec8fd0205
JinkerLeong <jinker2003@gmail.com>
2022-01-27 17:27:51 +0800

    except all exception

--- IPHelper.py
+++ IPHelper.py
@@ -4,6 +4,6 @@ from requests import get, exceptions
 def GetIPV4():
     try:
         result = get("https://ipv4.jsonip.com/", timeout=5).json()
-    except exceptions.RequestException as e:
+    except Exception as e:
         return "error"
     return result.get("ip")
